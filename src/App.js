99d59ca19b18e476962e40cf01107e17a303f664
Raman <watuwnnt@gmail.com>
2025-03-15 21:03:07 +0300

    add form and list

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import { useEffect } from 'react';
 import './App.css';
 import useTelegram from "./components/hooks/useTelegram.js";
 import Header from "./components/header/Header.jsx";
-
+import { Route, Routes } from 'react-router-dom';
+import ProductList from './ProductList/ProductList.jsx';
+import Form from './components/Form/Form.jsx';
 
 function App() {
   const {onToggleButton, tg} = useTelegram();
@@ -14,7 +16,10 @@ function App() {
   return (
   <div className='App'>
     <Header />
-    <button onClick={onToggleButton}>toggle</button>
+    <Routes>
+      <Route index element={<ProductList/>}/>
+      <Route path={'/form'} element={<Form/>}/>
+    </Routes>
   </div>)
 }
 
