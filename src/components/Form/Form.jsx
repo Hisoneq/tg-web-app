f704a0546e7bee16736d0cb8fd27d32f67909088
Raman <watuwnnt@gmail.com>
2025-03-15 22:18:42 +0300

    fixed data

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -15,14 +15,14 @@ export default function Form(){
             subject
         }
         tg.sendData(JSON.stringify(data));
-    }, [])
+    }, [country, street, subject])
 
     useEffect(()=>{
         tg.onEvent('mainButtonClicked', onSendData);
         return() => {
             tg.offEvent('mainButtonClicked', onSendData);
         }
-    }, [])
+    }, [onSendData])
 
     useEffect(()=>{
         tg.MainButton.setParams({
