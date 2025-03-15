179f96933698d918a6e29f8999e7fc82f24e99d2
Raman <watuwnnt@gmail.com>
2025-03-15 23:58:55 +0300

    added rofles

--- src/ProductList/ProductList.jsx
+++ src/ProductList/ProductList.jsx
@@ -6,14 +6,14 @@ import { useCallback, useEffect } from "react";
 
 
 const products = [
-    {id: 1, title: 'Куртка', price: 500, description: 'Зеленого цвета, теплая'},
-    {id: 2, title: 'Джинсы', price: 150, description: 'Синего цвета, дырявые'},
-    {id: 3, title: 'Трусы', price: 20, description: 'Черного цвета, обосранные'},
-    {id: 4, title: 'Перчатки', price: 5000, description: 'Как в ксго'},
-    {id: 5, title: 'Гандоны', price: 10, description: 'Вторяк'},
-    {id: 6, title: 'Шапка', price: 50, description: 'Обычная'},
-    {id: 7, title: 'Шарфик', price: 100, description: 'Белый, палинин'},
-    {id: 8, title: 'Дырка', price: 0, description: 'На жопе'}
+    {id: 1, title: 'Куртка', price: 500, description: 'Зеленого цвета, теплая', image: 'http://ecx.images-amazon.com/images/I/61mMrkwBFSL._UL1001_.jpg'},
+    {id: 2, title: 'Джинсы', price: 150, description: 'Синего цвета, дырявые', image: 'https://img.joomcdn.net/be004f390a937de33a621a9c8370b58616b583cb_original.jpeg'},
+    {id: 3, title: 'Трусы', price: 20, description: 'Белого цвета, обосранные', image: 'https://60.img.avito.st/image/1/1.FiVRTra4usxn53jJcSIff2fsuMrv7zjEJ-q4zuHnssbn.j9B7q__vTy935ogCKYLx4B2i8RnaR9cFEHTQM08dOf8'},
+    {id: 4, title: 'Перчатки', price: 5000, description: 'Как в ксго', image: 'https://dgvdyislmj77y.cloudfront.net/eyJidWNrZXQiOiJ0cmFkZWl0LXdpa2kiLCJrZXkiOiJpbWFnZXMvaXRlbXMvaGFuZC13cmFwcy1jYXV0aW9uLS1mYWN0b3J5LW5ldy5wbmciLCJlZGl0cyI6eyJyZXNpemUiOnsid2lkdGgiOjEyMDAsImhlaWdodCI6OTAwLCJmaXQiOiJjb250YWluIiwiYmFja2dyb3VuZCI6IiMyNzI3M2YifSwiZmxhdHRlbiI6eyJiYWNrZ3JvdW5kIjoiIzI3MjczZiJ9LCJvdmVybGF5V2l0aCI6eyJidWNrZXQiOiJ0cmFkZWl0LXdpa2kiLCJrZXkiOiJ3YXRlcm1hcmstbG9nby5wbmciLCJvcHRpb25zIjp7ImdyYXZpdHkiOiJzb3V0aHdlc3QifX19fQ=='},
+    {id: 5, title: 'Гандоны', price: 10, description: 'Вторяк', image: 'https://avatars.mds.yandex.net/i?id=74809b360ad1646d22f8b6163dc43bbec164b18a-3702239-images-thumbs&n=13'},
+    {id: 6, title: 'Шапка', price: 50, description: 'Обычная', image: 'https://smart-lab.ru/uploads/2022/images/02/78/63/2022/01/25/9dbb9e.jpg'},
+    {id: 7, title: 'Шарфик', price: 100, description: 'Белый, стрышный', image: 'https://images-eu.ssl-images-amazon.com/images/I/41KOB5Nm1uL.jpg'},
+    {id: 8, title: 'Дырка', price: 0, description: 'На жопе', image: 'https://i0.wp.com/motherlesspics.com/uploads/posts/2023-12/1702666946_motherlesspics-com-p-erotika-bolshie-dirki-24.jpg?ssl=1'}
 ]
 
 const getTotalPrice = (items) => {
--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -10,7 +10,7 @@ export default function ProductItem({product, className, onAdd}){
 
     return(
         <div className={'product ' + className}>
-            <div className={'img'} />
+            <img src={product.image} alt={product.title} className={'img'} />
             <div className={'title'}>{product.title}</div>
             <div className={'description'}>{product.description}</div> 
             <div className={'price'}>
